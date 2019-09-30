b4124e2f3b6a45158089879a394d16fe0b47b63d
Andres Vasquez <avasquez@anvargear.co>
2019-09-29 20:48:00 -0500

    Funtions download invoices

--- invoices/urls.py
+++ invoices/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path(r'v1/invoices', views.InvoicesAPIView.as_view(), name='invoices-list'),
+    path(r'v1/invoices/download', views.download_file, name='download'),
 ]
--- invoices/views.py
+++ invoices/views.py
@@ -1,3 +1,4 @@
+import csv
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import generics
@@ -9,3 +10,12 @@ class InvoicesAPIView(generics.ListCreateAPIView):
     queryset = Invoices.objects.all()
     serializer_class = InvoicesSerializer
 
+
+def download_file(request):
+    response = HttpResponse(content_type="text/csv")
+    response['Content-Disposition'] = 'attachment; filename="invoices-ID.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['nit', 'valor', 'fecha',])
+
+    return response
